1127323178bdd6f6f186728fd013be536899290a
zak905 <zakaria.amine88@gmail.com>
2017-02-08 20:33:51 +0100

    initial commit

---
+++ src/main/java/com/gwidgets/MyUI.java
@@ -0,0 +1,57 @@
+package com.gwidgets;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("runo")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        final TextArea message = new TextArea();
+        name.setCaption("name");
+        message.setCaption("message");
+        
+       
+        Button button = new Button("Send");
+       
+        
+        button.addClickListener( e -> {
+        	//We can use System.out since we are in the server side 
+        	System.out.println(name.getValue() + " says : ");
+        	System.out.println(message.getValue());
+        });
+        
+        
+        layout.addComponents(name, message, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
